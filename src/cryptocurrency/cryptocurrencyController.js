e96f1db01ff809b62f50efa23e9da9d50ae54704
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2019-05-27 22:19:43 +0200

    Adds get cmc map endpoint

--- src/cryptocurrency/cryptocurrencyController.js
+++ src/cryptocurrency/cryptocurrencyController.js
@@ -1,6 +1,17 @@
 import axios from "../axiosInstance";
 
-export async function getAllCryptocurrenciesLatest(req, res, next) {
+export async function getCoinMarketCapMap(req, res, next) {
+  try {
+    const response = await axios.get("/cryptocurrency/map");
+    const data = response.data.data;
+
+    res.status(200).send(data);
+  } catch (error) {
+    res.status(400).send({ exception: "general", error });
+  }
+}
+
+export async function listAllCryptocurrenciesLatest(req, res, next) {
   try {
     const response = await axios.get("/cryptocurrency/listings/latest");
     const data = response.data.data;
--- src/cryptocurrency/cryptocurrencyRoutes.js
+++ src/cryptocurrency/cryptocurrencyRoutes.js
@@ -1,13 +1,15 @@
 import express from "express";
 
 import {
-  getAllCryptocurrenciesLatest,
+  getCoinMarketCapMap,
+  listAllCryptocurrenciesLatest,
   getMarketQuotesLatest
 } from "./cryptocurrencyController";
 
 const router = express.Router();
 
-router.get("/listing/latest", getAllCryptocurrenciesLatest);
+router.get("/map", getCoinMarketCapMap);
+router.get("/listing/latest", listAllCryptocurrenciesLatest);
 router.get("/quotes/latest", getMarketQuotesLatest);
 
 export default router;
